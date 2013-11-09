82fe9f611a52cfc398e462e3c107a4bfc8d409bc
mhs294 <mhs294@mst.edu>
2013-11-09 13:47:24 -0600

    fixed project file references

---
+++ BitBoard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSClient
+{
+  class BitBoard
+  {
+    // length of each BitArray (width * height)
+    public static readonly int length;
+
+    // constant bitboards
+    public static readonly BitArray empty = new BitArray(length, false);
+    public static readonly BitArray full = new BitArray(length, true);
+
+    // occupancy bitboards
+
+  }
+}
